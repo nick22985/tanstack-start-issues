9868c76168d61b4c86ffcbce544ce389afb73b77
nick22985 <nick@nick22985.com>
2025-11-24 10:08:44 +1000

    feat: try passing context

--- src/start.tsx
+++ src/start.tsx
@@ -14,5 +14,13 @@ export const test3Middleware = startInstance
 	.server(async ({ next, context }) => {
 		console.log("context", context);
 
+		return next({ context: { auth: "test2", user: "test" } });
+	});
+
+export const test4Middleware = startInstance
+	.createMiddleware({ type: "function" })
+	.server(async ({ next, context }) => {
+		console.log("context", context);
+
 		return next();
 	});
